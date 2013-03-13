a1708da89009deb870aadf1d6455212067577c85
Carl Dawson <carl@invisible-gorilla.com>
2013-03-13 00:12:04 -0400

    Added admin controls

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 User.new({:email => "admin@gmail.com", :password => "password", :password_confirmation => "password" }).save(validate: false)
